e224732c9b839c8ecc397d7b56a0ad65f4d97fad
Zeduaz <zeduaz@gmail.com>
2015-01-03 13:15:40 +0100

    Date and degug mode reposition

--- src/main.c
+++ src/main.c
@@ -9,9 +9,9 @@
 #include "config.h"
 
 #define TIME_FRAME      (GRect(0, 6, 144, 168-6))
-#define DATE_FRAME      (GRect(1, 66, 144, 168-62))
+#define DATE_FRAME      (GRect(1, 64, 144, 168-62))
 #define WEATHER_FRAME   (GRect(0, 98, 144, 70))
-#define DEBUG_FRAME     (GRect(0, 82, 144, 15))
+#define DEBUG_FRAME     (GRect(0, 81, 144, 15))
 #define BATTERY_FRAME   (GRect(5, 0, 144, 8))
 
 /* Keep a pointer to the current weather data as a global variable */
