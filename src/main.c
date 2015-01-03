3737a3a6fce0dff29da97b60147f23c313dd6c95
Zeduaz <zeduaz@gmail.com>
2015-01-03 13:39:57 +0100

    Weather update interval changed to 20 min

--- src/main.c
+++ src/main.c
@@ -58,7 +58,7 @@ static void handle_tick(struct tm *tick_time, TimeUnits units_changed)
 
   // Refresh the weather info every 30 mins, targeting 18 mins after the hour (Yahoo updates around then)
   if ((units_changed & MINUTE_UNIT) && 
-      (tick_time->tm_min % 30 == 18) &&
+      (tick_time->tm_min % 20 == 18) &&
       !initial_request) {
     request_weather(weather_data);
   }
